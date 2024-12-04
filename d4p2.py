cea0bc3ba4fc1846c8742ae204905d013eaf5bcd
EmileBrt <bertelootemile@gmail.com>
2024-12-04 08:25:55 -0500

    day 4 done

--- d4p2.py
+++ d4p2.py
@@ -1,15 +1,29 @@
 with open("i4.txt") as f :
     data = f.readlines()
 
+# data = [
+# "MMMSXXMASM",
+# "MSAMXMSMSA",
+# "AMXSXMAAMM",
+# "MSAMASMSMX",
+# "XMASAMXAMM",
+# "XXAMMXXAMA",
+# "SMSMSASXSS",
+# "SAXAMASAAA",
+# "MAMMMXMMMM",
+# "MXMXAXMASX"
+# ]
 A_ind = []
 
+r = ["MS","SM"]
+s = 0
 for i in range(1,len(data)-1):
     for j in range(1,len(data[0])-1):
         if data[i][j] == "A":
             A_ind.append([i,j])
-s=0
+            lft = data[i+1][j+1]+data[i-1][j-1]
+            rgt = data[i-1][j+1]+data[i+1][j-1]
+            if (lft in r) and (rgt in r):
+                s += 1
 
-for i,x in A_ind:
-    if( ( data[A_ind[i][0]-1][A_ind[i][1]-1] != data[A_ind[i][0]+1][A_ind[i][1]+1] ) and ( data[A_ind[i][0]+1][A_ind[i][1]-1] != data[A_ind[i][0]-1][A_ind[i][1]+1] )):
-        s+=1
 print(f"{s= }")
