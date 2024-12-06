c37ea2062721ff6c4c9a79538f3e714c146b9032
EmileBrt <bertelootemile@gmail.com>
2024-12-07 00:31:57 +0100

    day 6 p2 done , messy code... BUT no ugly deepcopy and a restart at the n-1 position of the P1 ! could be faster with MP but im good with 3 min run time !

--- d6p2.py
+++ d6p2.py
@@ -4,14 +4,15 @@ from tqdm import tqdm
 with open("i6.txt") as f:
     data = f.readlines()
 
+for i in range(len(data)):
+    data[i] = list(data[i])
+
 dir = [
     [0,-1], ## starting position
     [1,0],
     [0,1],
     [-1,0]
 ]
-
-obstable_pos = []
 
 for j,y in enumerate(data):
     for i,x in enumerate(y):
@@ -20,11 +21,9 @@ for j,y in enumerate(data):
             start_y = j
             start_dir = dir[0]
 
-print(f"{start_x= } {start_y= }")
-
-
 s = 0
 r = []
+r2 = []
 
 pos_x  = start_x
 pos_y = start_y
@@ -38,39 +37,39 @@ while(True):
     else:
         if [pos_x,pos_y] not in r:
             r.append([pos_x,pos_y])
+            r2.append([pos_x,pos_y,pos_dir])
 
     if (data[pos_y + pos_dir[1]][pos_x + pos_dir[0]] == "#"):
         pos_dir = dir[(dir.index(pos_dir) + 1 ) % len(dir)]
     else:
         pos_y += pos_dir[1]
         pos_x += pos_dir[0]
+
+print(len(r))
         
-for i, p in tqdm(enumerate(r), total=len(r), desc="Processing"):
+for i, p in tqdm(enumerate(r2), initial=1, total=len(r2), desc="Processing"):
 
-    data_copy = deepcopy(data)
-    ## data_copy[p[0]][p[1]] = "#"
-    data_copy[p[0]] = list(data_copy[p[0]])
-    data_copy[p[0]][p[1]] = "#"
-    data_copy[p[0]] = "".join(data_copy[p[0]])
+    data[p[1]][p[0]] = "#" 
+    
+    pos_x = r2[i - 1][0]
+    pos_y = r2[i - 1][1]
+    pos_dir = r2[i - 1][2]
 
-    r2 = []
-
-    pos_x = start_x
-    pos_y = start_y
-    pos_dir = start_dir
+    r3 = []
 
     while(True):
-        cond1 = (0 <= (pos_x + pos_dir[0]) < len(data_copy[0]))
-        cond2 = (0 <= (pos_y + pos_dir[1]) < len(data_copy))
+        cond1 = (0 <= (pos_x + pos_dir[0]) < len(data[0]))
+        cond2 = (0 <= (pos_y + pos_dir[1]) < len(data))
         if( (False == cond1) or (False == cond2) ):
+            data[p[1]][p[0]] = "." 
             break
-        elif [pos_x,pos_y,pos_dir] in r2: # looping
+        elif [pos_x,pos_y,pos_dir] in r3: # looping
+            data[p[1]][p[0]] = "." 
             s += 1
             break
         else:
-            r2.append([pos_x,pos_y,pos_dir])
-
-        if (data_copy[pos_y + pos_dir[1]][pos_x + pos_dir[0]] == "#"):
+            r3.append([pos_x,pos_y,pos_dir])
+        if (data[pos_y + pos_dir[1]][pos_x + pos_dir[0]] == "#"):
             pos_dir = dir[(dir.index(pos_dir) + 1 ) % len(dir)]
         else:
             pos_y += pos_dir[1]
