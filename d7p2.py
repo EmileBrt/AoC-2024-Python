016b5cfafbe80fd2fd641cd021c810a8f539dbbd
EmileBrt <bertelootemile@gmail.com>
2024-12-07 10:31:38 +0100

    beautify

--- d7p2.py
+++ d7p2.py
@@ -13,8 +13,7 @@ def rec_search(goal, l):
     for x in l:
         if x > goal:
             return 0
-    
-    # LEFT TO RIGHT READ THE CHALLENGE YOU DUMBASS
+        
     m = l.pop(0)
     l1 = deepcopy(l)
     l1[0] *= m
@@ -22,10 +21,8 @@ def rec_search(goal, l):
     l2[0] += m
     l3 = deepcopy(l)
     l3[0] = int(str(m)+str(l3[0]))
-
     return max(rec_search(goal,l1 ), rec_search(goal,l2),rec_search(goal,l3 ))
 
-    
 for i,_ in enumerate(d):
     d[i] = d[i].split(": ")
     d[i][0] = int(d[i][0])
@@ -35,6 +32,5 @@ s=0
 for i, _ in tqdm(enumerate(d), total=len(d), desc="Processing"):
     if 1 == rec_search(d[i][0],d[i][1]):
         s += d[i][0]
-if s != 362646859298554:
-    print("You failed")
+
 print(f"{s= }")
